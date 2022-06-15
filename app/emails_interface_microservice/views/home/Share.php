ebc1d3bf9771af535a78cc5d15282dcb33ee69f4
biancabuzila <79212200+biancabuzila@users.noreply.github.com>
2022-06-15 21:36:31 +0300

    Add files via upload

---
+++ app/emails_interface_microservice/views/home/Share.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>EMPub</title>
+        <link rel="stylesheet" href="../../Publish_Style.css">
+    </head>
+    
+    <body>
+        <p class="paragraph">Share the link with your friends:</p>
+
+        <form>
+            <input type="text" id="link" value="Link">
+            <div class="button copylink">
+                <button type="submit" id="copylink" onclick="window.location.href='./My_Published_Emails.html'">Copy link</button>
+            </div>
+        </form>
+    </body>
+</html>
