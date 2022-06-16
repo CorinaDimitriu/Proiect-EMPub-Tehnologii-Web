a181a219a25587794307b58a767779628dc0cfba
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2022-06-16 06:25:34 +0300

    Add files via upload

--- app/emails_interface_microservice/controllers/DisplayUnpublishedEmails.php
+++ app/emails_interface_microservice/controllers/DisplayUnpublishedEmails.php
@@ -2,10 +2,10 @@
 require_once '../emails_interface_microservice/models/User.php';
 require_once '../emails_interface_microservice/models/Email.php';
 require_once '../emails_interface_microservice/models/MailCollection.php';
-class SendRequest extends Controller
+class DisplayUnpublishedEmails extends Controller
 {
-    public function index ($email = '') {
-        $request_as_string = "http://localhost:8181/public/GetUnpublishedEmails/index?email=".$email;
+    public function index ($email = '', $noPage = 1, $noSections = 4) {
+        $request_as_string = "http://localhost:8181/public/GetUnpublishedEmails/index?email=".$email.'&noPage='.$noPage.'&noSections='.$noSections;
         $c = curl_init ();
         curl_setopt ($c, CURLOPT_URL, $request_as_string);              // stabilim URL-ul serviciului
         curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);  // rezultatul cererii va fi disponibil ca șir de caractere
