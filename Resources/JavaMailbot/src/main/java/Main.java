3a34c705354007eb16d4b5328f21479e5c642c6a
CorinaDimitriu <corina.dimitriu.01@gmail.com>
2022-06-20 11:11:46 +0300

    First commit

---
+++ Resources/JavaMailbot/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javax.mail.MessagingException;
+
+public class Main {
+    public static void main(String[] args) throws MessagingException {
+        String username = "emailpublisher1@gmail.com";
+        String password = "smkzskskfganhqhs";
+
+        ConnectionProperties con = new ConnectionProperties(username, password);
+        if (args.length == 0) {
+            new GetMails(con);
+        } else if (args.length == 2) {
+            new SendMail(args[0], Integer.parseInt(args[1]), con);
+        } else {
+            System.out.println("Wrong number of parameters.");
+            System.exit(1);
+        }
+    }
+}
