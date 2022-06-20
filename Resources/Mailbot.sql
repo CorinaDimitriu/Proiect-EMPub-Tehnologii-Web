afab7163b4aa6f89087782033db6647e7c1b2809
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2022-06-20 11:26:05 +0300

    Add files via upload

--- Resources/Mailbot.sql
+++ Resources/Mailbot.sql
@@ -1,22 +1,22 @@
-DROP TABLE friendsmails
+DROP TABLE friendsmails;
 /
-DROP TABLE usermails
+DROP TABLE usermails;
 /
-DROP TABLE users
+DROP TABLE users;
 /
 
 CREATE TABLE users(
-    email VARCHAR2(320) PRIMARY KEY NOT NULL,
-    verification_code NUMBER(4)
+    email VARCHAR2(320) PRIMARY KEY,
+    verification_code VARCHAR2(100)
 )
 /
 
 CREATE TABLE usermails(
   user_email VARCHAR2(320),
   subject VARCHAR2(103),
-  content_email VARCHAR2(500),
-  published NUMBER(1),
-  privacy VARCHAR2(10),
+  content_email VARCHAR2(500) PRIMARY KEY,
+  published NUMBER(1) DEFAULT 0,
+  privacy VARCHAR2(10) DEFAULT 'Public',
   password VARCHAR2(100),
   duration DATE,
   CONSTRAINT check_yesno CHECK (published IN (0,1))
@@ -29,3 +29,6 @@ CREATE TABLE friendsmails(
     content_email VARCHAR2(500)
 )
 /
+
+select * from usermails;
+
