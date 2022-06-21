641885adf0d6f58179ab991276b7776ba5e3dc58
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2022-06-21 09:06:36 +0300

    Update Mailbot.sql

--- Resources/Mailbot.sql
+++ Resources/Mailbot.sql
@@ -90,26 +90,3 @@ CREATE TABLE friendsmails(
 /
 
 ALTER DATABASE SET TIME_ZONE = '+00:00';
-
-select * from usermails;
-select * from friendsmails;
-
-DELETE FROM FRIENDSMAILS;
-delete from usermails;
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa!','emailpublisher1@gmail.com_1.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa2!','emailpublisher1@gmail.com_2.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa3!','emailpublisher1@gmail.com_3.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa4!','emailpublisher1@gmail.com_4.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa5!','emailpublisher1@gmail.com_5.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa6!','emailpublisher1@gmail.com_6.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa7!','emailpublisher1@gmail.com_7.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('emailpublisher1@gmail.com','Vacanta frumoasa8!','emailpublisher1@gmail.com_8.html',0);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('dimitriu.corina@gmail.com','Vacanta frumoasa10!','dimitriu.corina@gmail.com_1.html',1);
-INSERT INTO USERMAILS(user_email,subject,content_email,published) VALUES('corina.dimitriu.01@gmail.com','Vacanta frumoasa11!','corina.dimitriu.01@gmail.com_1.html',1);
-commit;
-
-select TO_CHAR(duration,'YYYY-MM-DD HH24:MI:SS') from usermails;
-
-INSERT INTO friendsmails VALUES('emailpublisher1@gmail.com','dimitriu.corina@gmail.com_1.html');
-INSERT INTO friendsmails VALUES('emailpublisher1@gmail.com','corina.dimitriu.01@gmail.com_1.html');
-SELECT * FROM (SELECT ROWNUM rn,unu,doi,trei FROM (SELECT u.user_email unu,u.subject doi,u.content_email trei FROM friendsmails f JOIN usermails u ON f.content_email = u.content_email WHERE u.user_email = 'dimitriu.corina@gmail.com' AND f.user_email = 'emailpublisher1@gmail.com' AND published = 1 ORDER BY f.content_email DESC) t) WHERE rn < 7 AND rn > 0;
